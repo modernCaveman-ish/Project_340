f7895c961f5de782fda3e719c850bf27cf474fb4
haribunny <46565898+haribunny@users.noreply.github.com>
2021-05-08 15:06:28 +0300

    Update quads.c

--- phaseC-NEW/quads.c
+++ phaseC-NEW/quads.c
@@ -1,54 +1,35 @@
 #include "quads.h"
-
+#include "hashtbl.h"
 unsigned programVarOffset = 0;
-unsigned functionlocalOffset = 0;
+unsigned functionLocalOffset = 0; 
 unsigned formalArgOffset = 0;
 unsigned scopeSpaceCounter = 1;
 
-/*
+struct quad* quads = (struct quad*) 0;
+unsigned total = 0;
+unsigned int currQuad = 0;
 
-lvalue -> id{
-    symbol = ; //lookup $id or create new
-    $lvalue = newexpr(var_e);
-    $lvalue -> sym = symbol;
-}
+extern int yylineno;
 
-const -> number{
-    $const = newexpr(constnum_e);
-    $const -> numCost = $number;
-}
+//fix emit_iftableitem
 
-expr -> expr1 + expr2{
-    $expr = newexpr(arithexpr_e);
-    $expr -> sym = newtemp();
-    emit(add, $expr1, $expr2, $expr);
-}
-
-assignexpr->lvalue = expr{
-    emit(assign, $expr, NULL, $lvalue); //other checks and emits here as well
-}
-
-epxr* emit_iftableitem(expr* e){
+struct expr* emit_iftableitem(struct expr* e){
     if (e->type != tableitem_e)
         return e;
     else{
-        epxr* result = newexpr(var_e);
+        struct expr* result = newexpr(var_e);
         result->sym = newtemp();
-        emit(tablegetelem, e, e->index, result);
-
+        result->type = e->type;
+	result->strConst=result->sym->name;
+        emit(tablegetelem_op, e, e->index, result,0,yylineno);
         return result;
     }
 }
 
-*/
+unsigned int nextquad (void) {
+ return currQuad+1;
+ }
 
-void enterscopespace(void){++scopeSpaceCounter;}
-
-void exitscopespace(void){assert(scopeSpaceCounter>1); --scopeSpaceCounter;}
-
-struct quad*   quads = (struct quad*) 0;
-unsigned    total = 0;
-unsigned int    currQuad = 0;
 
 void expand(void){
 	assert (total==currQuad);
@@ -74,8 +55,8 @@ enum scopespace_t currscopespace(void){
 unsigned currscopeoffset (void){
     switch (currscopespace()){
         case programvar         : return programVarOffset;
-        case functionlocal      : return functionlocalOffset;
-        case formalarg          : return formalArgOffset;
+        case functionlocal      : return functionLocalOffset ;
+        rmalarg          : return formalArgOffset;
         default                 : assert(0);
     }
 }
@@ -83,13 +64,44 @@ unsigned currscopeoffset (void){
 void inccurrscopeoffset (void){
     switch (currscopespace()){
         case programvar          : ++programVarOffset; break;
-        case functionlocal      : ++functionlocalOffset; break;
+        case functionlocal      : ++functionLocalOffset ; break;
         case formalarg          : ++formalArgOffset; break;
         default                 : assert(0);
     }
 }
 
+void enterscopespace ()
+    { ++scopeSpaceCounter; }
 
+void exitscopespace ()
+    { assert(scopeSpaceCounter>1); --scopeSpaceCounter; }
+
+void resetformalargsoffset() 
+    { formalArgOffset = 0; }
+
+void resetfunctionlocalsoffset() 
+    { functionLocalOffset = 0; }
+
+void restorecurrscopeoffset (unsigned n) {
+    switch (currscopespace()) {
+      case programvar : programVarOffset = n; break;
+      case functionlocal : functionLocalOffset  = n; break; 
+      case formalarg  : formalArgOffset = n; break; 
+      default: assert(0);
+	}
+}
+
+unsigned nextquadlabel (void) 
+   { return currQuad; }
+
+void patchlabel (unsigned int quadNo, unsigned int label) { 
+   assert(quadNo - 1 < currQuad);
+   quads[quadNo - 1].label = label;
+   
+}
+
+
+//EMIT
 void emit(	iopcode op,
 			struct	expr* arg1,
 			struct	expr* arg2,
@@ -121,11 +133,10 @@ struct expr*lvalue_expr (SymbolTableEntry* sym){
 
     switch (sym->typet){
 
-    case var_s:			e->type=var_e;break;
-    case programfunc_s: 		e->type=programfunc_e;break;
-    case libraryfunc_s:	       e->type=libraryfunc_e;break;
+    case var_s: e->type=var_e;break;
+    case programfunc_s: e->type=programfunc_e;break;
+    case libraryfunc_s:	e->type=libraryfunc_e;break;
     default:assert(0);
-
     }
 
 return e;
@@ -141,28 +152,71 @@ char * newtempname() {
     sprintf(tempname,"_t%u",tempcounter);//opou to number prpei na
     char *newtempname2 = strdup(tempname);
     return newtempname2;
+    tempcounter++;
 }
 
 void resettemp() { tempcounter = 0; }
 
 extern int scope;
+
 struct SymbolTableEntry *newtemp() {
-    char *name = newtempname();
-    //sym = lookup(name, currscope());
-
+    char *name = newtempname(); 
     struct SymbolTableEntry* sym = SymTable_contains2(table, name, scope);
-
+    //sym = lookup(name, currscope());
+   
+  
     if (sym == NULL)
       //  return newsymbol(name);
-       return SymTable_put(table, name, 0, scope,  GLOBAL);//fix
+       return SymTable_put(table, name, yylineno, scope, GLOBAL);
 
     else
         return sym;
 }
 
 //add newexpr
-//fix emit_iftableitem
-//dimiourgiste mia print sta quads opws orizei to FAQ
+struct expr* newexpr (expr_t t) {
+	struct expr* e = (struct expr*) malloc(sizeof(struct expr)); 
+	memset(e, 0, sizeof(struct expr));
+	e->type = t;
+	return e;
+}
+
+struct expr* newexpr_conststring (char* s) {
+	struct expr* e = newexpr(consttring_e); 
+	e->strConst = strdup(s);
+	return e;
+}
+
+struct expr* newexpr_constnum (double i) {
+	struct expr* e = newexpr(constnum_e); 
+	e->numConst = i;
+	return e;
+}
+
+struct expr* newexpr_constbool (unsigned int b) {
+	struct expr* e = newexpr(constbool_e);
+		if(b == 1){
+			e->boolConst =1;
+	        }else{
+			e->boolConst = 0;
+		}
+	return e;
+}
+
+struct expr* newexpr_constnil () {
+	struct expr* e = newexpr(nil_e);
+	//e->boolConst = b;
+	return e;
+}
+
+struct expr* member_item (struct expr* lv, char* name) {
+	lv = emit_iftableitem(lv); // Emit code if r-value use of table item
+	struct expr* ti = newexpr(tableitem_e); // Make a new expression
+	ti->sym = lv->sym;
+	ti->strConst=ti->sym->name;
+	ti->index = newexpr_conststring(name); // Const string index
+	return ti;
+}
 
 struct expr* assignexpr_lvalue_expr(struct expr* lvalue, struct expr* exp){
     struct expr* assignexpr;
@@ -177,3 +231,139 @@ struct expr* assignexpr_lvalue_expr(struct expr* lvalue, struct expr* exp){
         emit(assign_op, lvalue, NULL, assignexpr, 0,0);
     }
 }
+
+
+char * opcode[]={ "assign_op", "add_op", "sub_op", 
+	"mul_op", "div_op", "mod_op", 
+	"uminus_op", "and_op", "or_op", 
+	"not_op", "if_eq_op", "if_noteq_op", 
+	"if_lesseq_op", "if_greatereq_op", "if_less_op", 
+	"if_greater_op", "call_op",  "param_op" 
+	"ret_op", "getretval_op", "funcstart_op", 
+	"funcend_op", "tablecreate_op", 
+	"tablegetelem_op"," tablesetelem_op", "jump_op"};
+
+
+void print_symbol(expr *e){
+    printf("%s", e->sym->name);
+}
+
+
+void print_num(expr *e){
+    printf("%f", e->numConst);
+}
+
+void print_string(expr *e){
+    printf("%s", e->strConst);
+}
+
+void printf_bool(expr * e){
+    printf("%s", e->boolConst==1?"true":"false");
+}
+
+void print_nil(expr * e){
+    printf("nil");
+}
+
+void (*expr_prints[12])(expr *) = {
+    print_symbol,
+    print_symbol,
+    print_symbol,
+    print_symbol,
+    print_symbol,
+    print_symbol,
+    print_symbol,
+    print_symbol,
+    print_num,
+    printf_bool,
+    print_string,
+    print_nil
+};
+
+
+
+
+void print_expr (struct expr *e) {
+    if(e == NULL){
+        printf("\t\t\t");
+    } else{
+        //analogws to type prepei na ektypwseis to katallilo pedio tou expr
+        //px constring_e ektypws to strconst
+     /*   if(expr->type == 8){ //print ta noymera
+         printf("%d\t\t\t", expr->numConst);
+        }  else if(expr->type == 10){
+            printf("%s\t\t\t", expr->strConst);
+        } else if(expr->type == 9){
+            printf("%c\t\t\t", expr->boolConst);
+        }*/
+        expr_prints[e->type](e);
+     printf("\t\t\t");
+    }
+}
+
+
+void print_labels(quad *q){
+
+//check and print the lable if eligible
+   struct quad *tmpquad = q;
+
+   if (tmpquad->op==jump_op||
+        tmpquad->op==if_greatereq_op||
+        tmpquad->op==if_less_op||
+        tmpquad->op==if_eq_op||tmpquad->op==if_lesseq_op||tmpquad->op==if_greater_op){
+
+       printf("%d\n",tmpquad->label);
+   }
+    
+    
+}
+
+void Quad_Print(){
+//quad# opcode,result,arg1,arg2,label
+	struct quad *tmpquad;
+	int i;
+    
+    printf("quad#\topcode\t\t\tresult\t\t\targ1\t\t\targ2\t\t\tlabel\t\t\t \n");
+	
+    for(i=0;i<currQuad;i++){
+        printf("%d\t",i);
+        
+        printf("%s\t\t\t", opcode[quads[i].op]);
+        if(quads[i].result != NULL){
+            print_expr(quads[i].result);
+            print_expr(quads[i].arg1);
+            print_expr(quads[i].arg2);
+        }
+        print_labels(&quads[i]);
+        printf("\n");
+    }
+}
+/*
+struct expr* make_call (expr* lv, expr* reversed_elist) {
+	struct expr* func = emit_iftableitem(lv);
+	while (reversed_elist) {
+		emit(param, reversed_elist, NULL, NULL);
+		reversed_elist = reversed_elist->next;
+	}
+	emit(call_op, func,NULL, NULL);
+	expr* result = newexpr(var_e);
+	result->sym = newtemp();
+	emit(getretval_op, NULL, NULL, result);
+	return result;
+}
+
+void comperror (char* format, ...);const char* context
+*/
+
+void check_arith(expr* e){
+    if  (e->type== constbool_e      ||
+        e->type == consttring_e     ||
+        e->type == nil_e            ||
+        e->type == newtable_e       ||
+        e->type == programfunc_e    ||
+        e->type == libraryfunc_e    ||
+        e->type == boolexpr_e)
+      printf("Illegal expr used \n");
+}
+
+
