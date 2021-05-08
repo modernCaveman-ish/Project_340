5e2e3b3cb43151f69884c6f178fd65b647ba374a
haribunny <46565898+haribunny@users.noreply.github.com>
2021-05-08 15:07:24 +0300

    Update quads.h

--- phaseC-NEW/quads.h
+++ phaseC-NEW/quads.h
@@ -4,12 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
+#include "hashtbl.h"
 #define EXPAND_SIZE 1024
 #define CURR_SIZE (total*sizeof(struct quad))
 #define NEW_SIZE (EXPAND_SIZE*sizeof(struct quad) + CURR_SIZE)
 
-//dinontai ap to frontisthrio
 
 typedef enum iopcode {
 	assign_op, add_op, sub_op, 
@@ -24,7 +23,7 @@ typedef enum iopcode {
 } iopcode;
 
 
-enum expr_t {
+typedef enum expr_t {
 	var_e,
 	tableitem_e,
 
@@ -36,29 +35,15 @@ enum expr_t {
 	assignexpr_e,
 	newtable_e,
 
-	costnum_e,
+	constnum_e,
 	constbool_e,
 	consttring_e,
 
 	nil_e,
-} ;
+}expr_t ;
 
 
-
-#include "hashtbl.h"
-/*
-struct symbol
-{
-	enum symbol_t			type;
-	char*				name;
-	enum scopespace_t		space;
-	unsigned			offset;
-	unsigned			line;	
-};*/
-
-
-
-struct expr{
+typedef struct expr{
 	enum expr_t type;
 	struct SymbolTableEntry* sym;
 	struct expr* index;
@@ -66,15 +51,61 @@ struct expr{
 	char* strConst;
 	unsigned char boolConst;
 	struct expr* next;
-};
+}expr;
 
-struct quad{
+typedef struct quad{
 	enum iopcode op;
 	struct expr* result;
 	struct expr* arg1;
 	struct expr* arg2;
 	unsigned int label;
 	unsigned int line;	
+}quad;
+
+struct call {
+	struct expr* elist;
+	unsigned char method;
+	char* name;
 };
-struct expr*lvalue_expr (SymbolTableEntry* sym);
+
+struct breaklist{
+	int numquad;
+	struct breakList* next;
+} ;
+
+struct continuelist{
+	int numquad;
+	struct continuelist* next;
+} ;
+
+
+void print_symbol(expr *e);
+void print_num(expr *e);
+void print_string(expr *e);
+void print_bool(expr * e);
+unsigned int nextquad(); 
+void Quad_Print();
+void expand();	
+struct expr* emit_iftableitem(struct expr* e);
+struct expr* lvalue_expr (SymbolTableEntry* sym);
+unsigned currscopeoffset (void);
+void inccurrscopeoffset (void);
+struct expr* newexpr_conststring (char* s);
+struct SymbolTableEntry *newtemp();
+char * newtempname() ;
+void resettemp();
+void emit(iopcode op,struct expr* arg1,struct expr* arg2,struct expr* result,unsigned label,unsigned line);
+void enterscopespace();
+void exitscopespace();
+void resetfunctionlocalsoffset();
+void resetformalargsoffset() ;
+struct expr* newexpr (expr_t t);
+expr* newexpr_conststring (char* s);
+expr* newexpr_constnum (double i);
+expr* newexpr_constbool (unsigned int b);
+struct expr* member_item (struct expr* lv, char* name);
+struct expr* assignexpr_lvalue_expr(struct expr* lvalue, struct expr* exp);
+void patchlabel (unsigned int quadNo, unsigned int label);
+struct expr* newexpr_constnil();
+
 #endif
