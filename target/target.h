16c01ea967e87c63156ae2eae7cceccddefb2242
haribunny <46565898+haribunny@users.noreply.github.com>
2021-06-01 20:43:02 +0300

    Update target.h

--- target/target.h
+++ target/target.h
@@ -1,9 +1,15 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "quads.h"
+
+
 #define AVM_STACKENV_SIZE 4
 #define AVM_STACKSIZE 4096
 #define AVM_WIPEOUT(m) memset(&(m), 0, sizeof(m)) 
-#define CURR_SIZE_T (total*sizeof(struct instruction))
+#define CURR_SIZE_T (totalInstr*sizeof(struct instruction))
 #define NEW_SIZE_T (EXPAND_SIZE*sizeof(struct instruction + CURR_SIZE_T)
+
 
 
 double* numConsts;
@@ -12,11 +18,11 @@ char** stringConsts ;
 unsigned int totalStringConsts ;
 char** namedLibfuncs ;
 unsigned int totalNamedLibfuncs ;
-userfunc** userFuncs ;
+//userfunc** userFuncs ;
 unsigned int totalUserFuncs ;
 
 
-enum vmopcode { 
+typedef enum vmopcode { 
 	assign_v, 	add_v,	sub_v,
 	mul_v,	div_v,	mod_v,
 	uminus_v,	and_v,	or_v,
@@ -26,7 +32,7 @@ enum vmopcode {
 	funcenter_v, funcexit_v,	newtable_v,
 	tablegetelem_v,	tablesetelem_v,	nop_v,
 	jump_v
-} ; 
+} vmopcode; 
 	
 typedef enum vmarg_t { 
 	label_a		=0,
@@ -43,59 +49,64 @@ typedef enum vmarg_t {
 } vmarg_t; 
 
 
-struct vmarg { 
+typedef struct vmarg { 
 	vmarg_t type; 
 	unsigned  val; 
-}; 
+}vmarg; 
 
-struct instruction { 
-	vmopcode opcode; 
-	vmarg result; 
-	vmarg arg1; 
-	vmarg arg2; 
+typedef struct instruction { 
+	enum vmopcode opcode; 
+	struct vmarg result; 
+	struct vmarg arg1; 
+	struct vmarg arg2; 
 	unsigned int srcLine; 
-}; 
+}instruction; 
 
-struct userfunc { 
+typedef struct userfunc { 
 	unsigned int address; 
 	unsigned int localSize; 
 	char* id; 
-}; 
+} userfunc;
 
-incomplete_jump* ij_head =(incomplete_jump*) 0;
 unsigned ij_total = 0;
 
-struct incomplete_jump {
+typedef struct incomplete_jump {
 	unsigned 	instrNo; // The jump instruction number.
 	unsigned    iaddress;// The i-code jump-target address.  
 	struct incomplete_jump* next;// A trivial linked list. 
-} ; 
+} incomplete_jump; 
 
-extern void generate_ADD (quad*);  
-extern void generate_SUB (quad*);   
-extern void generate_MUL (quad*);  
-extern void generate_DIV (quad*);   
-extern void generate_MOD (quad*); 
-extern void generate_NEWTABLE (quad*);  
-extern void generate_TABLEGETELM (quad*);   
-extern void generate_TABLESETELEM (quad*);  
-extern void generate_ASSIGN (quad*);   
-extern void generate_NOP (quad* );  
-extern void generate_JUMP (quad*);  
-extern void generate_IF_EQ (quad*);  
-extern void generate_IF_NOTEQ (quad*);  
-extern void generate_IF_GREATER (quad*);   
-extern void generate_IF_GREATEREQ (quad*);  
-extern void generate_IF_LESS (quad*);  
-extern void generate_IF_LESSEQ (quad*);  
-extern void generate_NOT (quad*);  
-extern void generate_OR (quad*);  
-extern void generate_PARAM (quad*);  
-extern void generate_CALL (quad*);  
-extern void generate_GETRETVAL (quad*);  
-extern void generate_FUNCSTART (quad* );  
-extern void generate_RETURN (quad* );  
-extern void generate_FUNCEND (quad* );
+incomplete_jump* ij_head =(incomplete_jump*) 0;
+
+
+extern void generate_ADD (quad* quad);  
+extern void generate_SUB (quad* quad);   
+extern void generate_MUL (quad* quad);  
+extern void generate_DIV (quad* quad);   
+extern void generate_MOD (quad* quad); 
+extern void generate_NEWTABLE (quad* quad);  
+extern void generate_TABLEGETELM (quad* quad);   
+extern void generate_TABLESETELEM (quad* quad);  
+extern void generate_ASSIGN (quad* quad);   
+extern void generate_NOP (quad* quad);  
+extern void generate_JUMP (quad* quad);  
+extern void generate_IF_EQ (quad* quad);  
+extern void generate_IF_NOTEQ (quad* quad);  
+extern void generate_IF_GREATER (quad* quad);   
+extern void generate_IF_GREATEREQ (quad* quad);  
+extern void generate_IF_LESS (quad* quad);  
+extern void generate_IF_LESSEQ (quad* quad);  
+extern void generate_NOT (quad* quad);  
+extern void generate_OR (quad* quad);  
+extern void generate_PARAM (quad* quad);  
+extern void generate_CALL (quad* quad);  
+extern void generate_GETRETVAL (quad* quad);  
+extern void generate_FUNCSTART (quad* quad);  
+extern void generate_RETURN (quad* quad);  
+extern void generate_FUNCEND (quad* quad);
+
 unsigned nextinstrlabel (void) ;
 void expand_t(void);
-void emit_vm(instruction t);
+void emit_vm(struct instruction *instruction);
+void make_operand (struct expr* e, vmarg *arg);
+void generate_relational (enum vmopcode op,struct quad *quad);
